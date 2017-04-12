c487781520b1b3157a4e2a514299d7e79ae04ba5
mseffner <mseffner@gmail.com>
2017-04-11 20:46:55 -0700

    first practice program in JavaScript

--- chapter1.js
+++ chapter1.js
@@ -1,3 +1,30 @@
-a = 21;
-b = a*2;
-console.log(b);
+const TAX_RATE = 0.07;
+const BASE_PHONE_PRICE = 99.99;
+const ACCESSORY_PRICE = 9.99;
+const SPENDING_LIMIT = 50;
+var funds = 4400;
+var phonesBought = 0;
+var accessoriesBought = 0;
+var fundsSpentOnAccessories = 0;
+
+while (funds > BASE_PHONE_PRICE) {
+    funds -= addTax(BASE_PHONE_PRICE, TAX_RATE);
+    phonesBought++;
+}
+
+while (funds > ACCESSORY_PRICE && fundsSpentOnAccessories < SPENDING_LIMIT) {
+    funds -= addTax(ACCESSORY_PRICE, TAX_RATE);
+    accessoriesBought++;
+}
+
+console.log("Phones purchased: " + phonesBought
++ "\nPrice of phones: " + phonesBought * BASE_PHONE_PRICE + "\nAccessories bought: " +
+accessoriesBought)
+
+function addTax(base, taxRate) {
+    return base * (1 + taxRate)
+}
+
+function formatMoney(amount) {
+    return "$" + amount.toFixed(2);
+}
